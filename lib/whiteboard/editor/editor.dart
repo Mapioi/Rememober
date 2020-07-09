74515195bfba26754baea4067f36e093f3621b46
Théo Takla <trakjohnsonweb@gmail.com>
2020-07-10 00:43:00 +0200

    Add basic canvas controls

--- lib/whiteboard/editor/editor.dart
+++ lib/whiteboard/editor/editor.dart
@@ -2,6 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:Rememober/whiteboard/editor/canvas.dart';
 import 'package:Rememober/whiteboard/editor/stroke.dart';
 
+class ToolBar extends StatefulWidget {
+  final currentTool;
+  final onToolChange;
+
+  ToolBar({@required this.currentTool, @required this.onToolChange});
+
+  @override
+  _ToolBarState createState() => _ToolBarState();
+}
+
+class _ToolBarState extends State<ToolBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: toolToIcon.keys
+          .map((tool) => IconButton(
+                icon: Icon(
+                  toolToIcon[tool],
+                  color: widget.currentTool == tool
+                      ? Colors.lightBlue
+                      : Colors.black,
+                ),
+                onPressed: () => setState(() {
+                  widget.onToolChange(tool);
+                }),
+              ))
+          .toList(),
+    );
+  }
+}
+
 class WhiteboardEditor extends StatefulWidget {
   final onExit;
 
@@ -12,7 +43,10 @@ class WhiteboardEditor extends StatefulWidget {
 }
 
 class _WhiteboardEditorState extends State<WhiteboardEditor> {
-  Paint _currentPaint = blackPenPaint;
+  double _currentThickness = 5.0;
+  Color _currentColor = Colors.black;
+  Tool _currentTool = Tool.Pen;
+  Paint _currentPaint = genPenPaint(Colors.black, 5.0);
   final _strokes = <Stroke>[];
 
   _onPanStart(DragStartDetails details) {
@@ -20,56 +54,91 @@ class _WhiteboardEditorState extends State<WhiteboardEditor> {
     final currentPath = Path();
     currentPath.moveTo(localPos.dx, localPos.dy);
     setState(() {
-      _strokes.add(Stroke(
-        path: currentPath,
-        paint: _currentPaint,
-      ));
+      if (writable(_currentTool)) {
+        _strokes.add(Stroke(
+          path: currentPath,
+          paint:
+              _currentTool == Tool.Pen ? _currentPaint : yellowHighlighterPaint,
+        ));
+      }
     });
   }
 
   _onPanUpdate(DragUpdateDetails details) {
     final localPos = details.localPosition;
     final currentPath = _strokes.last.path;
-    setState(() {
-      currentPath.lineTo(localPos.dx, localPos.dy);
-    });
+    if (writable(_currentTool)) {
+      setState(() {
+        currentPath.lineTo(localPos.dx, localPos.dy);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        backgroundColor: Colors.white,
         actions: [
+          DropdownButton<double>(
+            value: _currentThickness,
+            iconEnabledColor: Colors.black,
+            iconDisabledColor: Colors.grey,
+            iconSize: 24,
+            dropdownColor: Colors.white,
+            underline: Container(height: 2, color: Colors.black54),
+            onChanged: (double newValue) {
+              setState(() {
+                _currentThickness = newValue;
+                _currentPaint = genPenPaint(_currentColor, _currentThickness);
+              });
+            },
+            items: thicknesses.map((double e) {
+              return DropdownMenuItem<double>(
+                value: e,
+                child: Text(e.toInt().toString() + "px"),
+              );
+            }).toList(),
+          ),
+          Container(width: 10),
+          DropdownButton<Color>(
+            value: _currentColor,
+            iconEnabledColor: Colors.black,
+            iconDisabledColor: Colors.grey,
+            iconSize: 0,
+            dropdownColor: Colors.white,
+            underline: Container(
+              height: 2,
+              color: Colors.black54,
+            ),
+            onChanged: (Color e) {
+              setState(() {
+                _currentColor = e;
+                _currentPaint = genPenPaint(_currentColor, _currentThickness);
+              });
+            },
+            items: colors.map((Color e) {
+              return DropdownMenuItem<Color>(
+                value: e,
+                child: Container(height: 3, width: 50, color: e),
+              );
+            }).toList(),
+          ),
+          Container(width: 10),
+          ToolBar(
+            currentTool: _currentTool,
+            onToolChange: (tool) => {setState(() => _currentTool = tool)},
+          ),
+          IconButton(
+            icon: Icon(Icons.autorenew),
+            onPressed: () => setState(() {
+              _strokes.clear();
+            }),
+          ),
           IconButton(
             icon: Icon(Icons.close),
             onPressed: widget.onExit,
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.edit,
-              color: Colors.black,
-            ),
-            onPressed: () => setState(() {
-              _currentPaint = blackPenPaint;
-            }),
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.edit,
-              color: Colors.red,
-            ),
-            onPressed: () => setState(() {
-              _currentPaint = redPenPaint;
-            }),
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.highlight,
-              color: Colors.yellow,
-            ),
-            onPressed: () => setState(() {
-              _currentPaint = yellowHighlighterPaint;
-            }),
           ),
         ],
       ),
--- lib/whiteboard/editor/stroke.dart
+++ lib/whiteboard/editor/stroke.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+const colors = [
+  Colors.blue,
+  Colors.red,
+  Colors.green,
+  Colors.black
+];
+
+const thicknesses = [
+  1.0, 2.0, 3.0, 5.0, 10.0, 15.0, 20.0, 40.0
+];
+
 class Stroke {
   final Path path;
   final Paint paint;
@@ -7,21 +18,18 @@ class Stroke {
   Stroke({@required this.path, @required this.paint});
 }
 
-final blackPenPaint = Paint()
-  ..isAntiAlias = true
-  ..style = PaintingStyle.stroke
-  ..strokeCap = StrokeCap.round
-  ..strokeJoin = StrokeJoin.round
-  ..strokeWidth = 10.0
-  ..color = Colors.black;
+enum Tool { Pen, Highlighter, StrokeEraser, ZoneEraser, StrokeMove, ZoneMove }
 
-final redPenPaint = Paint()
-  ..isAntiAlias = true
-  ..style = PaintingStyle.stroke
-  ..strokeCap = StrokeCap.round
-  ..strokeJoin = StrokeJoin.round
-  ..strokeWidth = 10.0
-  ..color = Colors.red;
+Map<Tool,IconData> toolToIcon = {
+  Tool.Pen: Icons.edit,
+  Tool.Highlighter: Icons.highlight,
+  Tool.StrokeEraser: Icons.bug_report,
+  Tool.ZoneEraser: Icons.bug_report,
+  Tool.StrokeMove: Icons.open_with,
+  Tool.ZoneMove: Icons.open_with,
+};
+
+final writable = (tool) => tool == Tool.Pen || tool == Tool.Highlighter;
 
 final yellowHighlighterPaint = Paint()
   ..isAntiAlias = true
@@ -30,3 +38,13 @@ final yellowHighlighterPaint = Paint()
   ..strokeJoin = StrokeJoin.round
   ..strokeWidth = 100.0
   ..color = Color.fromRGBO(255, 193, 7, 0.5);
+
+Paint genPenPaint(Color color, double thickness) {
+  return Paint()
+    ..isAntiAlias = true
+    ..style = PaintingStyle.stroke
+    ..strokeCap = StrokeCap.round
+    ..strokeJoin = StrokeJoin.round
+    ..color = color
+    ..strokeWidth = thickness;
+}
